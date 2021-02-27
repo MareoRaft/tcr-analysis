5477a1ad9f1374f95f5781497627fad21ee2932d
Matthew Lancellotti <mvlancellotti@gmail.com>
2021-02-27 12:29:16 -0500

    create get_distance_ladder function

--- src/sample_distances.py
+++ src/sample_distances.py
@@ -28,3 +28,34 @@ def l2(a, b):
 
 def jaccard(a, b):
   return 1 - jaccard_index(a, b)
+
+
+# meta distance functions
+def get_distance_ladder(vectors, dist_func, max_gap):
+  '''
+  Given a list of vectors, a distance function, and a maximum gap, compute for each vector v_i its distance from the following vector v_i+1, from v_i+2, and so forth up to v_i+max_gap.
+  '''
+  vectors_dists = []
+  for i in range(len(vectors)):
+    vector_dists = []
+    for gap in range(1, max_gap + 1):
+      if i + gap < len(vectors):
+        dist = dist_func(vectors[i], vectors[i + gap])
+      else:
+        dist = None
+      vector_dists.append(dist)
+    vectors_dists.append(vector_dists)
+  return vectors_dists
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/sample_distances_test.py
+++ src/sample_distances_test.py
@@ -18,5 +18,17 @@ def test_jaccard_index():
   dist = jaccard_index(a, b)
   assert dist == 1 / 3
 
+def test_get_distance_ladder():
+  # vectors len 0
+  assert get_distance_ladder([], lambda a, b: 1, 3) == []
+  # vectors len 1
+  assert get_distance_ladder(['s'], lambda a, b: 1, 0) == [[]]
+  assert get_distance_ladder(['s'], lambda a, b: 1, 1) == [[None]]
+  # vectors len 2
+  assert get_distance_ladder(['s', 't'], lambda a, b: 1, 1) == [[1], [None]]
+  assert get_distance_ladder(['s', 't'], lambda a, b: 1, 2) == [[1, None], [None, None]]
+  # vectors len 3
+  assert get_distance_ladder(['s', 't', 7], lambda a, b: 1, 2) == [[1, 1], [1, None], [None, None]]
+
 if __name__ == '__main__':
   test_jaccard_index()
