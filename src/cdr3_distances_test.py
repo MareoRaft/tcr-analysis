d4056fe96e41c1487d118632356643da68867ebe
Matthew Lancellotti <mvlancellotti@gmail.com>
2021-03-04 22:08:31 -0500

    fix cdr3_distances_test.py since we changed the dist funcs to only take tuples

--- src/cdr3_distances_test.py
+++ src/cdr3_distances_test.py
@@ -1,21 +1,26 @@
 import numpy as np
 
-from distances import *
+from cdr3_distances import *
 
+# helpers
+t = tuple
+
+
+# tests
 def test_hamming():
   # 0 distances
-  assert hamming('', '') == 0
-  assert hamming('q', 'q') == 0
-  assert hamming('qs', 'qs') == 0
+  assert hamming(t(''), t('')) == 0
+  assert hamming(t('q'), t('q')) == 0
+  assert hamming(t('qs'), t('qs')) == 0
   # 1 distances
-  assert hamming('', 't') == 1
-  assert hamming('x', 'xy') == 1
-  assert hamming('x', 'r') == 1
-  assert hamming('ax', 'bx') == 1
+  assert hamming(t(''), t('t')) == 1
+  assert hamming(t('x'), t('xy')) == 1
+  assert hamming(t('x'), t('r')) == 1
+  assert hamming(t('ax'), t('bx')) == 1
   # 2 distances
-  assert hamming('tt', '') == 2
-  assert hamming('fg', 'uu') == 2
-  assert hamming('fg', 'u') == 2
+  assert hamming(t('tt'), t('')) == 2
+  assert hamming(t('fg'), t('uu')) == 2
+  assert hamming(t('fg'), t('u')) == 2
 
 def test_one_to_many():
   assert one_to_many('s', ['s'], hamming, min) == 0
