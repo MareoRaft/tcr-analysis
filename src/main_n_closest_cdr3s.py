523e33f4dc4d154cc7121afe27a7ce92aab7d5ce
Matthew Lancellotti <mvlancellotti@gmail.com>
2021-04-22 13:11:35 -0400

    add frontend for main_n_closest_cdr3s. working.

--- src/main_n_closest_cdr3s.py
+++ src/main_n_closest_cdr3s.py
@@ -12,32 +12,40 @@ from pretty_print import pprint
 
 
 def get_closest_cdr3s(cdr3, cdr3s, dist_func, limit):
+    ''' internal function '''
     cdr3_to_dist = {c:dist_func(c,cdr3) for c in cdr3s}
     sorted_cdr3_to_dist = sorted(cdr3_to_dist.items(), key=lambda item: item[1])
     top_n_cdr3_to_dist = sorted_cdr3_to_dist[:limit]
     return top_n_cdr3_to_dist
 
 def get_closest_cdr3s_with_frequency(cdr3, sample, dist_func, limit):
+    ''' internal function '''
     cdr3_to_dist = {c:(dist_func(c,cdr3),f) for c,f in sample.items()}
     sorted_cdr3_to_dist = sorted(cdr3_to_dist.items(), key=lambda item: (item[1][0],-item[1][1]))
     top_n_cdr3_to_dist = sorted_cdr3_to_dist[:limit]
     return top_n_cdr3_to_dist
 
-@record_elapsed_time
-def main():
-    file_name = 'cdr3.a.A_2017_2018_d_00_53535.ann'
+def get_n_closest_cdr3s(file_name, cdr3, n, dist_func, n_gram_len=1):
+    ''' user facing function '''
     sample = data_utils.get_cdr3_counter_from_file('s', file_name)
-    cdr3 = 'cVVSAFQAGTALIf'
-    n = 20
-    n_gram_len = 1
-    dist_func = on_n_gram(n_gram_len)(cdr3_distances.hamming)
+    ngrammed_dist_func = on_n_gram(n_gram_len)(dist_func)
     closest_cdr3s = get_closest_cdr3s_with_frequency(
         cdr3=cdr3,
         sample=sample,
-        dist_func=dist_func,
+        dist_func=ngrammed_dist_func,
         limit=n,
     )
     pprint(closest_cdr3s)
+
+@record_elapsed_time
+def main():
+    get_n_closest_cdr3s(
+        file_name='cdr3.a.A_2017_2018_d_00_53535.ann',
+        cdr3='cVVSAFQAGTALIf',
+        n=20,
+        dist_func=cdr3_distances.hamming,
+        n_gram_len=1,
+    )
 
 if __name__ == '__main__':
   main()
