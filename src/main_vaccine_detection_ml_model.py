2b8054ae503d16f88df4f4fda4216eb925b09a0a
Matthew Lancellotti <mvlancellotti@gmail.com>
2021-05-04 17:29:40 -0400

    add leave one out cross validation, and accuracy. works.

--- src/main_vaccine_detection_ml_model.py
+++ src/main_vaccine_detection_ml_model.py
@@ -8,32 +8,21 @@ import pandas as pd
 from sklearn.linear_model import SGDClassifier
 from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, LeaveOneOut
+import sklearn
 
 from decorators import record_elapsed_time
 import data_utils
 from pretty_print import pprint
 
 
-@record_elapsed_time
-def is_vaccinated(X, y):
-    ''' internal function. '''
-    # data
-    X = pd.DataFrame.from_dict(X, orient='index').fillna(0)
-    print(X)
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=1, random_state=None)
-    print('train:')
-    print(X_train)
-    print('test:')
-    print(X_test)
-    # note: docs say 'for best results, the data should have 0 mean and unit variance'
-    # note: features may need to be converted to floats
-    # TODO: example says 'ALWAYS SCALE INPUT'
-    # setup model
-    # SGDClassifier uses a Linear SVM by default, and SGD optimization to train it.
+def run_model(X_train, X_test, y_train, y_test):
+    # shuffle data
+    X_train, y_train = sklearn.utils.shuffle(X_train, y_train, random_state=None) # untested
+    # init model
     classifier = make_pipeline(
         StandardScaler(),
-        SGDClassifier(max_iter=10000),
+        SGDClassifier(max_iter=4000),
     )
     # train model
     classifier.fit(X_train, y_train)
@@ -41,14 +30,37 @@ def is_vaccinated(X, y):
     y_test_pred = classifier.predict(X_test)
     return y_test, y_test_pred
 
+@record_elapsed_time
+def run_loocv(X, y):
+    ''' internal function. '''
+    # data
+    num_correct = 0
+    num_total = 0
+    results = []
+    X = pd.DataFrame.from_dict(X, orient='index').fillna(0)
+    y = np.array(y)
+    # X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=1, random_state=None)
+    loo = LeaveOneOut()
+    for train_index, test_index in loo.split(X):
+        X_train = X.loc[train_index]
+        X_test = X.loc[test_index]
+        y_train = y[train_index]
+        y_test = y[test_index]
+        y_test, y_pred = run_model(X_train, X_test, y_train, y_test)
+        num_correct += bool(y_test == y_pred)
+        num_total += 1
+        results.append((y_test, y_pred))
+    accuracy = num_correct / num_total
+    return results, accuracy
+
 def detect_vaccine(X, y):
     ''' user facing function '''
     # remove test
     # convert file names to sample vectors
     X = {index:data_utils.get_cdr3_counter_from_file(f,f) for index,f in enumerate(X)}
-    y_test, y_pred = is_vaccinated(X, y)
-    print(y_test)
-    print(y_pred)
+    results, accuracy = run_loocv(X, y)
+    print(results)
+    print(accuracy)
 
 @record_elapsed_time
 def main():
@@ -58,24 +70,24 @@ def main():
             'cdr3.a.A_2017_2018_d_07_11143.ann',
             'cdr3.a.B_2017_2018_d_00_56786.ann',
             'cdr3.a.B_2017_2018_d_09_50844.ann',
-            # 'cdr3.a.C_2017_2018_d_00_26898.ann',
-            # 'cdr3.a.C_2017_2018_d_07_48996.ann',
-            # 'cdr3.a.D_2017_2018_d_00_45294.ann',
-            # 'cdr3.a.D_2017_2018_d_07_55841.ann',
-            # 'cdr3.a.E_2017_2018_d_00_94077.ann',
-            # 'cdr3.a.E_2017_2018_d_07_54569.ann',
+            'cdr3.a.C_2017_2018_d_00_26898.ann',
+            'cdr3.a.C_2017_2018_d_07_48996.ann',
+            'cdr3.a.D_2017_2018_d_00_45294.ann',
+            'cdr3.a.D_2017_2018_d_07_55841.ann',
+            'cdr3.a.E_2017_2018_d_00_94077.ann',
+            'cdr3.a.E_2017_2018_d_07_54569.ann',
         ],
         y=[
             0,
             1,
             0,
             1,
-            # 0,
-            # 1,
-            # 0,
-            # 1,
-            # 0,
-            # 1,
+            0,
+            1,
+            0,
+            1,
+            0,
+            1,
         ],
     )
 
